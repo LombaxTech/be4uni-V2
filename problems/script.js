b2d41f0caded8927b488794a49ff000ff3c0e433
Rakib Khan <rakibkhan@live.co.uk>
2019-02-24 17:34:21 +0000

    first commit

---
+++ problems/script.js
@@ -0,0 +1,66 @@
+let ans1 = document.querySelector('.answer1');
+let ans2 = document.querySelector('.answer2');
+let ans3 = document.querySelector('.answer3');
+let ans4 = document.querySelector('.answer4');
+let ans5 = document.querySelector('.answer5');
+let ans6 = document.querySelector('.answer6');
+let ans7 = document.querySelector('.answer7');
+// console.log(ans1);
+
+function showAns1(){
+    if (ans1.style.display === "none"){
+    ans1.style.display = "block";
+    }
+    else{
+        ans1.style.display = "none";
+    }
+}
+
+function showAns2(){
+    if (ans2.style.display === "none"){
+    ans2.style.display = "block";
+    }
+    else{
+        ans2.style.display = "none";
+    }
+}
+function showAns3(){
+    if (ans3.style.display === "none"){
+    ans3.style.display = "block";
+    }
+    else{
+        ans3.style.display = "none";
+    }
+}
+function showAns4(){
+    if (ans4.style.display === "none"){
+    ans4.style.display = "block";
+    }
+    else{
+        ans4.style.display = "none";
+    }
+}
+function showAns5(){
+    if (ans5.style.display === "none"){
+    ans5.style.display = "block";
+    }
+    else{
+        ans5.style.display = "none";
+    }
+}
+function showAns6(){
+    if (ans6.style.display === "none"){
+    ans6.style.display = "block";
+    }
+    else{
+        ans6.style.display = "none";
+    }
+}
+function showAns7(){
+    if (ans7.style.display === "none"){
+    ans7.style.display = "block";
+    }
+    else{
+        ans7.style.display = "none";
+    }
+}
